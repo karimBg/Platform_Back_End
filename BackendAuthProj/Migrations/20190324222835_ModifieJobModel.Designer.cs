2fd93d08bd2155255e0ef2fc2f0cf5ad5385e40f
Mohamed Karim Boughammoura <boughammoura.karim@gmail.com>
2019-03-24 23:30:27 +0100

    Add new Migration(ModifieJobModel ) and apply it to the database

--- BackendAuthProj/Migrations/20190324222835_ModifieJobModel.Designer.cs
+++ BackendAuthProj/Migrations/20190324222835_ModifieJobModel.Designer.cs
@@ -4,14 +4,16 @@ using BackendAuthProj.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BackendAuthProj.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190324222835_ModifieJobModel")]
+    partial class ModifieJobModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
